60619f86e4f93826b0366c32082b8663e9a574e0
wrivera-tenerum <88796320+wrivera-tenerum@users.noreply.github.com>
2022-12-19 13:57:11 -0500

    Update RemoteDirsync.ps1

--- RemoteDirsync.ps1
+++ RemoteDirsync.ps1
@@ -1,5 +1,5 @@
 # This script allows us to initiate a forced DirSync from our local workstation. 
-# It requires the -AdminName parameter is passed with the ym\username admin cred. 
+# It requires the -AdminName parameter is passed with the domain\username admin cred. 
 # To run Dirsync (remotely or otherwise), the user needs to be part of the FIMAdmins of FimSyncAdmins group in AD.
 
 
@@ -9,7 +9,7 @@ param (
 
 
 #Attributes
-$DirsyncServer = "srv-stp-dc03.corp.yourmembership.com"  # FQDN of the Dirsync Server
+$DirsyncServer = "srv-stp-dc03.some_domain_name"  # FQDN of the Dirsync Server
 
 $path = "C:\Users\$env:username\Documents\WindowsPowerShell\" # The Folder to store the credentials (with the trailing \)
 $CredsFile = $path + "YMPowershellCreds.txt" # The file that will contain the securestring
